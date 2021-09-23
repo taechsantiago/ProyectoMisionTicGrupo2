6611a5c6638810cd680da285be4cfba9e77952fc
stivison <sticruz@gmail.com>
2021-09-23 16:53:44 -0500

    se agrega la funciòn de validar_nombre_usuario

--- validaciones.js
+++ validaciones.js
@@ -3,4 +3,11 @@ function validar_contrasena(string){
     return regex.test(string);
 }
 
+function validar_nombre_usuario(string) {
+    const regex = /^[^0-9 ]{1}([A-Z ])+[A-Z]+$/;
+    return regex.test(string);
+}
+
+
+module.exports.validar_nombre_usuario =validar_nombre_usuario;
 module.exports.validar_contrasena = validar_contrasena;
